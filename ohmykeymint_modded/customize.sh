4faf1aab020a8b3589dd39bf0409b2078e22b0c0
Az-Har Muhamad Nayif <azharmuhamadnayif@gmail.com>
2026-05-31 22:57:23 +0700

    Oh My Keymint Modded

--- ohmykeymint_modded/customize.sh
+++ ohmykeymint_modded/customize.sh
@@ -1,5 +1,4 @@
 # shellcheck disable=SC2034
-SKIPUNZIP=1
 
 SONAME="Oh My Keymint"
 SUPPORTED_ABIS="arm64 arm64-v8a"
@@ -48,30 +47,8 @@ else
   ui_print "- Device sdk: $API"
 fi
 
-ui_print "- Extracting verify.sh"
-unzip -o "$ZIPFILE" 'verify.sh' -d "$TMPDIR" >&2
-if [ ! -f "$TMPDIR/verify.sh" ]; then
-  ui_print "*********************************************************"
-  ui_print "! Unable to extract verify.sh!"
-  ui_print "! This zip may be corrupted, please try downloading again"
-  abort    "*********************************************************"
-fi
-. "$TMPDIR/verify.sh"
-extract "$ZIPFILE" 'customize.sh'  "$TMPDIR/.vunzip"
-extract "$ZIPFILE" 'verify.sh'     "$TMPDIR/.vunzip"
-
-ui_print "- Extracting module files"
-extract "$ZIPFILE" 'module.prop'     "$MODPATH"
-extract "$ZIPFILE" 'post-fs-data.sh' "$MODPATH"
-extract "$ZIPFILE" 'service.sh'      "$MODPATH"
-extract "$ZIPFILE" 'sepolicy.rule'   "$MODPATH"
-extract "$ZIPFILE" 'daemon'          "$MODPATH"
-extract "$ZIPFILE" 'daemon-injector' "$MODPATH"
-extract "$ZIPFILE" 'injector.toml'   "$MODPATH"
-extract "$ZIPFILE" 'keybox.xml'      "$MODPATH"
 chmod 755 "$MODPATH/daemon" "$MODPATH/daemon-injector" \
-  "$MODPATH/post-fs-data.sh" "$MODPATH/service.sh"
-
+  "$MODPATH/post-fs-data.sh" "$MODPATH/service.sh" 
 
 if [ "$ARCH" = "x64" ] || [ "$ARCH" = "x86_64" ]; then
   ui_print "- Using packaged x64 binaries"
